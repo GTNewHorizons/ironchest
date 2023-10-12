1b770c802d006110faf91139fb41c32aa874f647
Nilau <nico.spiske@gmail.com>
2023-10-12 15:41:34 +0200

    [minor] added bookpark pull feature to ironchests, bumped deps and bs, sa applied

---
+++ src/main/java/cpw/mods/ironchest/plugins/nei/NEIConfig.java
@@ -0,0 +1,24 @@
+package cpw.mods.ironchest.plugins.nei;
+
+import codechicken.nei.DefaultBookmarkContainerHandler;
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import cpw.mods.ironchest.client.GUIChest;
+
+public class NEIConfig implements IConfigureNEI {
+
+    @Override
+    public void loadConfig() {
+        API.registerBookmarkContainerHandler(GUIChest.class, new DefaultBookmarkContainerHandler());
+    }
+
+    @Override
+    public String getName() {
+        return "Iron Chests";
+    }
+
+    @Override
+    public String getVersion() {
+        return "${version}";
+    }
+}
