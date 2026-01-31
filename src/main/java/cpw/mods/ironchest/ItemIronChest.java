2db8989df4b1034ad00815bec36befd2f71d8836
Eldrinn-Elantey <46845681+Eldrinn-Elantey@users.noreply.github.com>
2026-01-31 04:39:01 +0400

    Add tooltips for Iron Chest item variants

--- src/main/java/cpw/mods/ironchest/ItemIronChest.java
+++ src/main/java/cpw/mods/ironchest/ItemIronChest.java
@@ -7,9 +7,16 @@
  ******************************************************************************/
 package cpw.mods.ironchest;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemIronChest extends ItemBlock {
 
@@ -27,5 +34,34 @@ public class ItemIronChest extends ItemBlock {
     @Override
     public String getUnlocalizedName(ItemStack itemstack) {
         return "tile.ironchest:" + IronChestType.values()[itemstack.getItemDamage()].name();
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean advanced) {
+        switch (stack.getItemDamage()) {
+            case 0:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:IRON.tooltip"));
+                break;
+            case 1:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:GOLD.tooltip"));
+                break;
+            case 2:
+            case 5:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:DIAMOND.tooltip"));
+                break;
+            case 3:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:COPPER.tooltip"));
+                break;
+            case 4:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:STEEL.tooltip"));
+                break;
+            case 6:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:OBSIDIAN.tooltip"));
+                break;
+            case 9:
+                tooltip.add(StatCollector.translateToLocal("tile.ironchest:DARKSTEEL.tooltip"));
+                break;
+        }
     }
 }
